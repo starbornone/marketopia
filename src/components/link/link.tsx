649dc0716b432f7edcfadbd452bf9774efbab691
Sha <sha.kongbrooks@gmail.com>
2024-10-24 16:05:01 +1100

    Add appearance to Link

--- src/components/link/link.tsx
+++ src/components/link/link.tsx
@@ -3,17 +3,27 @@ import styles from "./link.module.scss";
 import { default as NextLink } from "next/link";
 
 interface LinkProps {
+  appearance?: "primary" | "secondary";
   children: React.ReactNode;
   disabled?: boolean;
   href: string;
   size?: "large" | "small";
 }
 
-export const Link = ({ children, href, size }: LinkProps) => {
+export const Link = ({
+  appearance = "primary",
+  children,
+  href,
+  size,
+}: LinkProps) => {
   return (
     <NextLink
       href={href}
-      className={clsx(styles["link"], styles[`link--size-${size}`])}
+      className={clsx(
+        styles["link"],
+        styles[`link--size-${size}`],
+        styles[`link--appearance-${appearance}`]
+      )}
     >
       {children}
     </NextLink>
